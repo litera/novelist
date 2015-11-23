6d2b047a8b4029d597bd0afeb2ad6d2df057e1d8
Robert Koritnik <robert.koritnik@gmail.com>
2015-11-23 09:00:20 +0100

    Updated ApplicationController routing to support Angular HTML5 mode

--- Web/Controllers/ApplicationController.cs
+++ Web/Controllers/ApplicationController.cs
@@ -8,6 +8,7 @@ namespace Novelist.Web.Controllers
 	{
 		// GET: Application
 		[Route]
+		[Route("{*catchall}")] // we need this to make it possible for Angular html5Mode to work as expected
 		public ActionResult Start()
 		{
 			return View();
